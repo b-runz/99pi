772f3711da0e3ec859e6cb419ac2888a817b7b43
b-runz <bjarke.runz@outlook.dk>
2023-10-11 10:42:28 +0200

    Added Radiolab feed

---
+++ func/src/functions/GetRlabRSSFeed.ts
@@ -0,0 +1,77 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import axios from "axios";
+import { parseString, Builder } from "xml2js";
+
+const parseXmlAsync = (xmlData: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    parseString(xmlData, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+export async function Get99RSSFeedV2(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  try {
+    const rssUrl = "https://feeds.feedburner.com/radiolab";
+
+    context.log("Fetching RSS podcast feed from:", rssUrl);
+
+    // Fetch the RSS feed
+    const response = await axios.get(rssUrl);
+
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch RSS feed.");
+    }
+
+    context.log("RSS podcast feed fetched successfully.");
+
+    // Parse the XML response
+    const xmlData = response.data;
+    const result = await parseXmlAsync(xmlData);
+
+    // Replace the URL within the enclosure tag with a URL-encoded version
+    if (result.rss && result.rss.channel) {
+      result.rss.channel[0].title = "Rlab"
+      if (result.rss.channel[0].item) {
+        const items = result.rss.channel[0].item;
+        for (const item of items) {
+          if (item.enclosure && item.enclosure[0].$.url) {
+            const originalUrl = item.enclosure[0].$.url;
+            const encodedUrl =
+              "https://jolly-water-052fedd03.3.azurestaticapps.net/api/UrlProxy?url=" +
+              encodeURIComponent(originalUrl);
+            item.enclosure[0].$.url = encodedUrl;
+          }
+        }
+      }
+    }
+
+    // Build the modified XML data
+    const builder = new Builder();
+    const modifiedXml = builder.buildObject(result);
+
+    return {
+      body: modifiedXml,
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    };
+
+
+  } catch (error) {
+    return {
+      status: 500,
+      body: "Internal Server Error",
+    };
+  }
+};
+
+app.http("Get99RSSFeedV2", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: Get99RSSFeedV2,
+});
